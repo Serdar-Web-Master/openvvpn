d89d78587e86a983f0a8990344b06c458bf11ce1
Serdar-Web-Master <serdarcholukov@yandex.com>
2022-04-01 00:45:38 +0500

    alert

--- js/user.js
+++ js/user.js
@@ -1,10 +1,18 @@
-const input__user = document.querySelector ('.input__user');
-const user__button = document.querySelector ('.user__button');
-const use__4 = document.querySelector ('.use__4');
-const use__5 = document.querySelector ('.use__5');
+const input__user = document.querySelector(".input__user");
+const user__button = document.querySelector(".user__button");
+const use__4 = document.querySelector(".use__4");
+const use__5 = document.querySelector(".use__5");
 
-user__button.addEventListener('click',displayBlock);
+user__button.addEventListener("click", displayBlock);
 
-function displayBlock(){
-    use__4.classList.add('block')
+function displayBlock() {
+  const password = input__user.value;
+  if (password === 777) {
+    use__4.classList.add("block");
+  }
+  if (password === 888) {
+    use__5.classList.add("block");
+  } else {
+    alert("pasword");
+  }
 }
