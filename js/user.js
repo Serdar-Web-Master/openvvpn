2ed67af4ffff7916d5154e3c8751e0c1be6d8feb
Serdar-Web-Master <serdarcholukov@yandex.com>
2022-04-01 00:22:27 +0500

    user 4-5

--- js/user.js
+++ js/user.js
@@ -1,9 +1,10 @@
 const input__user = document.querySelector ('.input__user');
 const user__button = document.querySelector ('.user__button');
 const use__4 = document.querySelector ('.use__4');
+const use__5 = document.querySelector ('.use__5');
 
-user__button.addEventListener('click',f1);
+user__button.addEventListener('click',displayBlock);
 
-function f1(){
+function displayBlock(){
     use__4.classList.add('block')
 }
